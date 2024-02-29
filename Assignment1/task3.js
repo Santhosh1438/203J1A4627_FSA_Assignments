0db744447c8a08e1272e8d0ab62ea0ea4c34d964
Santhosh1438 <142676111+Santhosh1438@users.noreply.github.com>
2024-03-01 00:38:59 +0530

    Create task3.js

---
+++ Assignment1/task3.js
@@ -0,0 +1,11 @@
+console.log("Interfaces");
+;
+var user = function (point) {
+    console.log(point);
+    console.log(point.name);
+};
+user({
+    name: 'santhosh',
+    age: 20,
+    email: 'santhoshkaredla1438@gmail.com'
+});
